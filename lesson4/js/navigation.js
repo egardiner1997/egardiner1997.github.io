71108525258644e177932e61fe06ed7f2a65cc33
elizabethgardiner18@gmail.com <43352210+ForgottenEbony@users.noreply.github.com>
2020-10-06 23:50:31 -0400

    commit new images and text

--- lesson4/js/navigation.js
+++
@@ -1,5 +0,0 @@
-
-const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.summary')
-
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
