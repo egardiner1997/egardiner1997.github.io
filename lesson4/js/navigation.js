2b09cb9eecef9bcfdd8ea16a5e8f4b0be71f9eed
elizabethgardiner18@gmail.com <43352210+ForgottenEbony@users.noreply.github.com>
2020-10-06 20:58:26 -0400

    changes

---
+++ lesson4/js/navigation.js
@@ -0,0 +1,5 @@
+
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.summary')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
