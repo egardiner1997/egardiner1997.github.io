78d939b69a6f79cc83c0e85b78d46fc0c2294980
elizabethgardiner18@gmail.com <43352210+ForgottenEbony@users.noreply.github.com>
2020-10-12 12:30:19 -0400

    changes lesson5

---
+++ lesson5/js/scripts.js
@@ -0,0 +1,11 @@
+let d = new Date();
+let year = d.getFullYear();
+document.getElementById("year").textContent = year;
+
+let modified = document.lastModified;
+document.getElementById("modified").textContent = modified;
+
+
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
