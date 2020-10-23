53d9505ccab5b7007042e97dfc6dac91dd029604
egardiner1997 <elizabethgardiner18@gmail.com>
2020-10-23 17:54:25 -0400

    3

--- lesson6/js/footer.js
+++ lesson6/js/footer.js
@@ -1,7 +1,6 @@
 let dateElement = document.getElementById('date-right-now');
 let date = new Date();
 let formatOptions = {
-  weekday: 'long',
   year: 'numeric',
   month: 'long',
   day: 'numeric'
