c330dcd04403fb28de94b13cf5908b396ada4bfe
egardiner1997 <elizabethgardiner18@gmail.com>
2020-11-16 10:17:47 -0500

    changes to weatherapi-10

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,6 +1,12 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=={864026d837b6f9641c1d5d20c101245b}";
+const apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=864026d837b6f9641c1d5d20c101245b";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
+    document.getElementById('current-temp').textContent = jsObject.list[0].main.temp;
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.list[0].weather[0].icon + '.png';  // note the concatenation
+    const desc = jsObject.list[0].weather[0].description;  // note how we reference the weather array
+    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+    document.getElementById('icon').setAttribute('alt', desc);
   });
