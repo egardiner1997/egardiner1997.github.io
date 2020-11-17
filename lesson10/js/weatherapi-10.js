3c85b011f0880243f40c8447049b77d4c6aa7f32
egardiner1997 <elizabethgardiner18@gmail.com>
2020-11-17 15:18:06 -0500

    changes made to lesson10

--- lesson10/js/weatherapi-10.js
+++ lesson10/js/weatherapi-10.js
@@ -6,26 +6,42 @@ fetch(apiURL)
   .then((jsObject) => {
     console.log(jsObject);
 
-    document.getElementById("weather").textContent = jsObject.list[6].weather[0].main;
-    document.getElementById('high').textContent = Math.round(jsObject.list[6].main.temp_max);
-    document.getElementById('temp').textContent = Math.round(jsObject.list[6].main.temp);
-    document.getElementById('humid').textContent = jsObject.list[6].main.humidity;
-    document.getElementById('wSpeed').textContent = jsObject.list[6].wind.speed;
+    document.getElementById("weather").textContent = jsObject.list[0].weather[0].main;
+    document.getElementById('high').textContent = Math.round(jsObject.list[0].main.temp_max);
+    document.getElementById('temp').textContent = Math.round(jsObject.list[0].main.temp);
+    document.getElementById('humid').textContent = jsObject.list[0].main.humidity;
+    document.getElementById('wSpeed').textContent = Math.round(jsObject.list[0].wind.speed);
 
-    console.log("Dyanmiac Weather Summary");
+    console.log("Dynamic Weather Summary");
 }); 
+
+  //This is for the 5 Day Forecast Headers
+  fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+  console.log(jsObject);    
+      //Using dt_txt for the day titles to make them dynamic
+    document.getElementById('day').textContent = jsObject.list[0].dt_txt;
+    document.getElementById('day2').textContent = jsObject.list[1].dt_txt;
+    document.getElementById('day3').textContent = jsObject.list[9].dt_txt;
+    document.getElementById('day4').textContent = jsObject.list[17].dt_txt;
+    document.getElementById('day5').textContent = jsObject.list[25].dt_txt;
+
+    console.log("5 Day Forecast Headers");
+    });
+
 
   //This is for the 5 Day Forecast Temperatures
 fetch(apiURL)
 .then((response) => response.json())
 .then((jsObject) => {
 console.log(jsObject);    
-    //Find all the ones with 18:00:00 as the dt_text for the 5 Day Forecast
-    document.getElementById('monday').textContent = Math.round(jsObject.list[6].main.temp_max);
-    document.getElementById('tuesday').textContent = Math.round(jsObject.list[14].main.temp_max);
-    document.getElementById('wednesday').textContent = Math.round(jsObject.list[22].main.temp_max);
-    document.getElementById('thursday').textContent = Math.round(jsObject.list[30].main.temp_max);
-    document.getElementById('friday').textContent = Math.round(jsObject.list[38].main.temp_max);
+    //Using main.temp_max for the highest temperature of the day
+    document.getElementById('temper').textContent = Math.round(jsObject.list[0].main.temp_max);
+    document.getElementById('temper2').textContent = Math.round(jsObject.list[1].main.temp_max);
+    document.getElementById('temper3').textContent = Math.round(jsObject.list[9].main.temp_max);
+    document.getElementById('temper4').textContent = Math.round(jsObject.list[17].main.temp_max);
+    document.getElementById('temper5').textContent = Math.round(jsObject.list[25].main.temp_max);
 
 
     console.log("5 Day Forecast Temperatures");
@@ -37,45 +53,45 @@ fetch(apiURL)
 .then((response) => response.json())
 .then((jsObject) => {
     console.log(jsObject);    
-    //Find all the ones with 18:00:00 as the dt_text for the images
-    //Monday
-    const image = 'https://openweathermap.org/img/w/' + jsObject.list[6].weather[0].icon + '.png';  // note the concatenation
-    const desc = jsObject.list[6].weather[0].description;  // note how we reference the weather array
+    //Using weather.icon for the icon images
+    //Day 1
+    const image = 'https://openweathermap.org/img/w/' + jsObject.list[0].weather[0].icon + '.png';  // note the concatenation
+    const desc = jsObject.list[0].weather[0].description;  // note how we reference the weather array
     document.getElementById('image').textContent = image; // informational specification only, shows the link of the icon
     document.getElementById('icon').setAttribute('src', image);  // focus on the setAttribute() method
     document.getElementById('icon').setAttribute('alt', desc);
 
 
-    //Tuesday
-    const image1 = 'https://openweathermap.org/img/w/' + jsObject.list[14].weather[0].icon + '.png';  // note the concatenation
-    const desc1 = jsObject.list[14].weather[0].description;  // note how we reference the weather array
-    document.getElementById('image1').textContent = image1; // informational specification only, shows the link of the icon
-    document.getElementById('icon1').setAttribute('src', image1);  // focus on the setAttribute() method
-    document.getElementById('icon1').setAttribute('alt', desc1);
-
-
-    //Wednesday
-    const image2 = 'https://openweathermap.org/img/w/' + jsObject.list[22].weather[0].icon + '.png';  // note the concatenation
-    const desc2 = jsObject.list[22].weather[0].description;  // note how we reference the weather array
+    //Day 2
+    const image2 = 'https://openweathermap.org/img/w/' + jsObject.list[1].weather[0].icon + '.png';  // note the concatenation
+    const desc2 = jsObject.list[1].weather[0].description;  // note how we reference the weather array
     document.getElementById('image2').textContent = image2; // informational specification only, shows the link of the icon
     document.getElementById('icon2').setAttribute('src', image2);  // focus on the setAttribute() method
     document.getElementById('icon2').setAttribute('alt', desc2);
 
 
-    //Thursday
-    const image3 = 'https://openweathermap.org/img/w/' + jsObject.list[30].weather[0].icon + '.png';  // note the concatenation
-    const desc3 = jsObject.list[30].weather[0].description;  // note how we reference the weather array
+    //Day 3
+    const image3 = 'https://openweathermap.org/img/w/' + jsObject.list[9].weather[0].icon + '.png';  // note the concatenation
+    const desc3 = jsObject.list[9].weather[0].description;  // note how we reference the weather array
     document.getElementById('image3').textContent = image3; // informational specification only, shows the link of the icon
     document.getElementById('icon3').setAttribute('src', image3);  // focus on the setAttribute() method
     document.getElementById('icon3').setAttribute('alt', desc3);
 
 
-    //Friday
-    const image4 = 'https://openweathermap.org/img/w/' + jsObject.list[38].weather[0].icon + '.png';  // note the concatenation
-    const desc4 = jsObject.list[38].weather[0].description;  // note how we reference the weather array
+    //Day 4
+    const image4 = 'https://openweathermap.org/img/w/' + jsObject.list[17].weather[0].icon + '.png';  // note the concatenation
+    const desc4 = jsObject.list[17].weather[0].description;  // note how we reference the weather array
     document.getElementById('image4').textContent = image4; // informational specification only, shows the link of the icon
     document.getElementById('icon4').setAttribute('src', image4);  // focus on the setAttribute() method
     document.getElementById('icon4').setAttribute('alt', desc4);
+
+
+    //Day 5
+    const image5 = 'https://openweathermap.org/img/w/' + jsObject.list[25].weather[0].icon + '.png';  // note the concatenation
+    const desc5 = jsObject.list[25].weather[0].description;  // note how we reference the weather array
+    document.getElementById('image5').textContent = image5; // informational specification only, shows the link of the icon
+    document.getElementById('icon5').setAttribute('src', image5);  // focus on the setAttribute() method
+    document.getElementById('icon5').setAttribute('alt', desc5);
 
     console.log("5 Day Forecast Images");
 });
