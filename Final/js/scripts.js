ba4e7fa698328147ed7cb5e5246fe829baa91581
egardiner1997 <elizabethgardiner18@gmail.com>
2020-12-07 13:22:47 -0500

    changes to index.html

--- Final/js/scripts.js
+++ Final/js/scripts.js
@@ -1,15 +1,3 @@
-//banner script
-let today = new Date();
-let day = today.getDay();
-
-if (day == 5) {
-    console.log("this is the right day");
-    document.getElementById("pancakes").style.display = "block";
-} else {
-    console.log("this is not the right day");
-    document.getElementById("pancakes").style.display = "none";
-}
-
 //navigation script
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
