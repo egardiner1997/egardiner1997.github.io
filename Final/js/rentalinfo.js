c12da8363480379aaf1f43b647b3570fe833b852
egardiner1997 <elizabethgardiner18@gmail.com>
2020-12-07 18:19:42 -0500

    changes added and finished for rental.html

--- Final/js/rentalinfo.js
+++ Final/js/rentalinfo.js
@@ -1,29 +1,49 @@
-const requestURL = 'js/info.json';
+const apiURL = "js/info.json";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
 
+    document.getElementById("name").textContent = jsObject.info[0].scooters[0].name;
+    document.getElementById('persons').textContent = jsObject.info[0].scooters[0].maxpersons;
+    document.getElementById('halfdayA').textContent = jsObject.info[0].scooters[0].reserved[0].half;
+    document.getElementById('fulldayA').textContent = jsObject.info[0].scooters[0].reserved[1].full;
+    document.getElementById('halfdayB').textContent = jsObject.info[0].scooters[0].walkin[0].half;
+    document.getElementById('fulldayB').textContent = jsObject.info[0].scooters[0].walkin[1].full;
 
-fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const rentalinfo = jsonObject['info'];
-    for (let i = 0; i < rentalinfo.length; i++ ) {
-        let card = document.createElement('section');
-        let h2 = document.createElement('h2');
-        let line = document.createElement('h4');
-        let photo = document.createElement('img');
+    document.getElementById("name2").textContent = jsObject.info[0].scooters[1].name;
+    document.getElementById('persons2').textContent = jsObject.info[0].scooters[1].maxpersons;
+    document.getElementById('halfdayA2').textContent = jsObject.info[0].scooters[1].reserved[0].half;
+    document.getElementById('fulldayA2').textContent = jsObject.info[0].scooters[1].reserved[1].full;
+    document.getElementById('halfdayB2').textContent = jsObject.info[0].scooters[1].walkin[0].half;
+    document.getElementById('fulldayB2').textContent = jsObject.info[0].scooters[1].walkin[1].full;
 
-        h2.textContent = jsonObject.info[i].type;
-        card.appendChild(h2);
+    document.getElementById("name3").textContent = jsObject.info[0].scooters[2].name;
+    document.getElementById('persons3').textContent = jsObject.info[0].scooters[2].maxpersons;
+    document.getElementById('halfdayA3').textContent = jsObject.info[0].scooters[2].reserved[0].half;
+    document.getElementById('fulldayA3').textContent = jsObject.info[0].scooters[2].reserved[1].full;
+    document.getElementById('halfdayB3').textContent = jsObject.info[0].scooters[2].walkin[0].half;
+    document.getElementById('fulldayB3').textContent = jsObject.info[0].scooters[2].walkin[1].full;
 
-        line.textContent = jsonObject.info[i].motto;
-        card.appendChild(line);
+    document.getElementById("name4").textContent = jsObject.info[1].atvs[0].name;
+    document.getElementById('persons4').textContent = jsObject.info[1].atvs[0].maxpersons;
+    document.getElementById('halfdayA4').textContent = jsObject.info[1].atvs[0].reserved[0].half;
+    document.getElementById('fulldayA4').textContent = jsObject.info[1].atvs[0].reserved[1].full;
+    document.getElementById('halfdayB4').textContent = jsObject.info[1].atvs[0].walkin[0].half;
+    document.getElementById('fulldayB4').textContent = jsObject.info[1].atvs[0].walkin[1].full;
 
-        photo.setAttribute('src', jsonObject.info[i].photo);
-        card.appendChild(photo);
+    document.getElementById("name5").textContent = jsObject.info[2].jeeps[0].name;
+    document.getElementById('persons5').textContent = jsObject.info[2].jeeps[0].maxpersons;
+    document.getElementById('halfdayA5').textContent = jsObject.info[2].jeeps[0].reserved[0].half;
+    document.getElementById('fulldayA5').textContent = jsObject.info[2].jeeps[0].reserved[1].full;
+    document.getElementById('halfdayB5').textContent = jsObject.info[2].jeeps[0].walkin[0].half;
+    document.getElementById('fulldayB5').textContent = jsObject.info[2].jeeps[0].walkin[1].full;
 
+    document.getElementById("name6").textContent = jsObject.info[2].jeeps[1].name;
+    document.getElementById('persons6').textContent = jsObject.info[2].jeeps[1].maxpersons;
+    document.getElementById('halfdayA6').textContent = jsObject.info[2].jeeps[1].reserved[0].half;
+    document.getElementById('fulldayA6').textContent = jsObject.info[2].jeeps[1].reserved[1].full;
+    document.getElementById('halfdayB6').textContent = jsObject.info[2].jeeps[1].walkin[0].half;
+    document.getElementById('fulldayB6').textContent = jsObject.info[2].jeeps[1].walkin[1].full;
 
-        document.querySelector('div.cards').appendChild(card);
-    }
-  });
+});
