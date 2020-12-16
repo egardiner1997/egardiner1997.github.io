1cfe40037759723ce5e238c1296d531e29cf015a
egardiner1997 <elizabethgardiner18@gmail.com>
2020-12-16 00:02:56 -0500

    changes to script

--- Final/js/scripts.js
+++ Final/js/scripts.js
@@ -3,18 +3,9 @@ function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
 }
 
-//footer date script
-//const format = {
-  //  weekday: 'long',
-   // day: 'numeric',
-    //month: 'long',
-    //year: 'numeric'
-  //};
-  //document.getElementById('date-right-now').textContent = new Date().toLocaleDateString('en-us', format);
-
 let d = new Date();
 let year = d.getFullYear();
 document.getElementById("year").textContent = year;
 
 let modified = document.lastModified;
-document.getElementById("modified").textContent = modified;
+document.getElementById("modified").textContent = "Last Modified: " + modified;
